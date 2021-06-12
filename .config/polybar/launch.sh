9d40831692ff17cf09a9ab9e9efd85d7a4f22309
DragonMeido <srmajhi00@gmail.com>
2021-06-12 13:02:44 +0530

    more dotfile !!#

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar main;
+#polybar bar2;
+
+echo "Bars launched..."
