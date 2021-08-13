fd95a64b3ed84ff690fd86df4bca721b252dbc25
DragonMeido <srmajhi00@gmail.com>
2021-08-13 13:07:07 +0530

    updated dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,4 +6,10 @@ if [ -f $HOME/.config/fish/aliases.fish ]
     source $HOME/.config/fish/aliases.fish
 end
 
+# repaint screen on screen resize
+function __fish_winch_handler --on-signal winch
+	commandline -f repaint
+end
+
 export EDITOR="nvim"
+
